cb532659ac6b2a603cc1f4220da1c75d6250919e
User <User@DESKTOP-DQVN2MB>
2021-12-07 22:19:38 +0200

    add the map

---
+++ TravelExperience.MVC/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelExperience.MVC.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Location
+        public ActionResult Map()
+        {
+            return View();
+        }
+    }
+}
