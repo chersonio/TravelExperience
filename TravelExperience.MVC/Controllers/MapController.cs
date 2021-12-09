4a88c9af12da75dec83d3281f3d5d79978205d02
User <User@DESKTOP-DQVN2MB>
2021-12-10 00:04:55 +0200

    refactor map view

--- TravelExperience.MVC/Controllers/MapController.cs
+++ TravelExperience.MVC/Controllers/MapController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelExperience.DataAccess.Core.Interfaces;
 
 namespace TravelExperience.MVC.Controllers
 {
     public class MapController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MapController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
         // GET: Location
         public ActionResult Map()
         {
             return View();
         }
+
+        public JsonResult GetLocation()
+        {
+
+            var data = _unitOfWork.Locations.GetAll();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+
+
     }
 }
