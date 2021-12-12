a4b51c0bc8070aa7bfad376979d727faed2e0664
Stavros Chersoniotakis <chersoniotakis.stavros@gmail.com>
2021-12-13 00:32:22 +0200

    Fixed an issue that the project did not load.

--- TravelExperience.MVC/Controllers/AccommodationController.cs
+++ TravelExperience.MVC/Controllers/AccommodationController.cs
@@ -74,7 +74,7 @@ namespace TravelExperience.MVC.Controllers
                 MaxCapacity = viewModel.Accommodation.MaxCapacity,
                 Shared = viewModel.Accommodation.Shared,
                 Floor = viewModel.Accommodation.Floor,
-                Thumbnail = viewModel.Accommodation.Thumbnail,
+                Thumbnail = viewModel.Accommodation.Thumbnail, // mporei na antikatastathei me to var path parakatw. Prepei na mpei pinakas me photo
                 AccommodationType = viewModel.AccommodationType, // this needs changing
                 HostID = userId,
                 Host = host
@@ -108,13 +108,12 @@ namespace TravelExperience.MVC.Controllers
             string path = "";
             if (viewModel.Image != null)
             {
-
                 // need to check how to save in specific folders depending on accommodation.
-                string pic = System.IO.Path.GetFileName(viewModel.Image.FileName);
-                path = System.IO.Path.Combine(
+                string pic = Path.GetFileName(viewModel.Image.FileName);
+                path = Path.Combine(
                                        Server.MapPath("~/images/profile"), pic);
 
-                // DO NOT DELETE
+                // ---DO NOT DELETE ---
 
                 // save the image path path to the database or you can send image 
                 // directly to database
@@ -125,6 +124,7 @@ namespace TravelExperience.MVC.Controllers
                 //    byte[] array = ms.GetBuffer();
                 //}
             }
+            //else { throw exception; }
 
             _unitOfWork.Bookings.Create(booking);
             _unitOfWork.Locations.Create(location);
@@ -136,16 +136,15 @@ namespace TravelExperience.MVC.Controllers
             }
             catch
             {
-
                 // anti exception na rixnei oti kati pige straba.
                 throw new Exception();
             }
 
             // file is uploaded here
-            if (path != null)
+            if (!string.IsNullOrWhiteSpace(path))
                 viewModel.Image.SaveAs(path);
 
-            return RedirectToAction("Location", "Accommodation"); // this needs to redirect to the area of the hosts accommodations
+            return RedirectToAction("Location", "Accommodation"); // this needs to redirect to the area of the hosts accommodations (Dashboard)
         }
 
         [HttpGet]
