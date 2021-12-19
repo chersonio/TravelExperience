87b9ba663281f63e8ed20d8cfb90f39ceae61f98
Stavros Chersoniotakis <69953403+chersonio@users.noreply.github.com>
2021-12-19 16:54:24 +0200

    Update AccommodationFormViewModel.cs

--- TravelExperience.MVC/ViewModels/AccommodationFormViewModel.cs
+++ TravelExperience.MVC/ViewModels/AccommodationFormViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Web;
@@ -12,20 +12,12 @@ namespace TravelExperience.MVC.ViewModels
 {
     public class AccommodationFormViewModel
     {
-
+        public Booking Booking { get; set; }
         public Accommodation Accommodation { get; set; }
         public ApplicationUser User { get; set; }
-        //public IEnumerable<AccommodationType> AccommodationTypes { get; set; }
-        //public SelectList AccommodationTypesSelectList { get; set; } // MultiSelectList to hold the options 
-        public AccommodationType AccommodationType { get; set; }
-        public IEnumerable<SelectListItem> AccommodationTypesList { get; set; }
+
         public Location Location { get; set; }
-
-        [AllowedFileExtension(".jpg", ".png", ".gif", ".jpeg")]
-        public HttpPostedFileBase Image { get; set; }
-
-
-
+        public HttpPostedFileBase Thumbnail { get; set; }
 
         // Utilities 
         public class UtilityForCheckbox // needed for checkbox
@@ -37,26 +29,7 @@ namespace TravelExperience.MVC.ViewModels
 
         public List<UtilityForCheckbox> UtilitiesForCheckboxes { get; set; } // needed for checkbox
         public List<Utility> Utilities { get; set; }
+        public List<Location> Locations { get; set; }
 
-    }
-    public class AllowedFileExtensionAttribute : ValidationAttribute
-    {
-        public string[] AllowedFileExtensions { get; private set; }
-        public AllowedFileExtensionAttribute(params string[] allowedFileExtensions)
-        {
-            AllowedFileExtensions = allowedFileExtensions;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            var file = value as HttpPostedFileBase;
-            if (file != null)
-            {
-                if (!AllowedFileExtensions.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase)))
-                {
-                    return new ValidationResult(string.Format("{1} için izin verilen dosya uzantıları : {0} : {2}", string.Join(", ", AllowedFileExtensions), validationContext.DisplayName, this.ErrorMessage));
-                }
-            }
-            return null;
-        }
     }
 }
